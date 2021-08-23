cecb0b4f3151c6cb8ada14e63a82c2c994102ad8
chrishoran8 <horan_chris@hotmail.co.uk>
2021-08-23 13:55:15 +0100

    Add project files.

---
+++ PythonEssentialsExample/PythonEssentialsExample.py
@@ -0,0 +1,23 @@
+#example python assessment
+
+#q1) - create a variable called numOne and assign it with a float of 2
+
+#2) Create another varaible called numTwo and assign it with a float of 5
+
+#3) create a third variable called numSum which captures the sum of numOne and numTwo
+
+#4) take an input from the user called numUser
+
+#5)a create a variable called highNumber with the value assigned to 100
+
+#5b perform a decision within the code which will output "High Number" 
+#if the product of numUser and numSum is greater than highNumber
+
+#6) create a list called firstList with the numbers 1-20 stored in it
+
+#7) create a second list called secondList which stores the value of the corresponding 
+#index in the first list multiplied by 2. e.g. [2,4,6,8....]
+
+
+#8) create a third list called boolList which stores whether the each element in secondList
+#is higher than highNumber (True) or lower (False)
