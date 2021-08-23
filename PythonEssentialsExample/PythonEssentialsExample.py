199e366b5058be6bca94c9871482dc7906a2462a
chrishoran8 <horan_chris@hotmail.co.uk>
2021-08-23 15:58:47 +0100

    added question 9

--- PythonEssentialsExample/PythonEssentialsExample.py
+++ PythonEssentialsExample/PythonEssentialsExample.py
@@ -14,11 +14,21 @@
 #if the product of numUser and numSum is greater than highNumber
 #and "Low Number" if smaller than 10 
 
-#6) create a list called firstList with the numbers 1-20 stored in it
+#6) using a for loop create a list called firstList with the numbers 1-20 stored in it
 
 #7) create a second list called secondList which stores the value of the corresponding 
 #index in the first list multiplied by 2. e.g. [2,4,6,8....]
 
 
-#8) create a third list called boolList which stores whether the each element in secondList
+#8) create a third list called boolList which stores whether each element in secondList
 #is higher than highNumber (True) or lower (False)
+
+
+
+#Question 9 will be once we have completed Mod 41 and Mod 42 review Tuesday
+
+#9a) create a func called numPrint that outputs a list of numbers between a starting 
+#number and a finish number. The function should have two parameters startNumber and endNumber
+#
+#9b) once you have done 9a, make a change so that if the user doesnt input an end number 
+#it automatically uses 10
